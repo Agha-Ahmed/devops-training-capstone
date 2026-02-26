b6250a21015d0e5f55ea681cfa7d408fb6868d05
Agha Ahmed <ahmed.mehmood@arbisoft.com>
2026-02-26 11:29:45 +0500

    Create a Dockerfile that builds a test runner container

---
+++ Week-3/Day-3/tests/test_app.py
@@ -0,0 +1,12 @@
+import requests
+
+def test_home():
+    r = requests.get("http://web:5000/")
+    assert r.status_code == 200
+    assert "Hello" in r.text
+
+def test_health():
+    r = requests.get("http://web:5000/health")
+    assert r.status_code == 200
+    # Flask returns JSON
+    assert r.json()["status"] == "healthy"
