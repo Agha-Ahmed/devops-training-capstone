d6a3ca5eee3b4bd41c465579d31171c29cb691ab
Agha Ahmed <ahmed.mehmood@arbisoft.com>
2026-03-08 10:52:15 +0500

    test: simulate failing deployment again

--- Week-3/Day-4/web/app.py
+++ Week-3/Day-4/web/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    raise Exception("Deployment test failure")
+    raise Exception("Deployment test failure!!!!")
 
 @app.route("/health")
 def health():
